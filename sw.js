d909b47a5125abdcf91dddaed72eac379362ef46
Dev-AmmarAhmed <slife7003@gmail.com>
2026-09-22 12:33:21 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-// sw.js - Service Worker for Native Notifications
 self.addEventListener('push', function(event) {
     console.log('[Service Worker] Push Received.');
 });
 self.addEventListener('notificationclick', function(event) {
     event.notification.close();
 });
+
 
 
 const CACHE_NAME = 'digital-classes-v1';
